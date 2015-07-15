320ba718c2d0ce6dcd107a9703f8d5ab2f08cfef
kamrul07 <kamrulhasanshuvo@outlook.com>
2015-07-16 01:42:11 +0600

    smile faces
    smile faces

--- newhompage1.php
+++ newhompage1.php
@@ -23,8 +23,7 @@
 
 
 		<!-- Carousel -->
-			<section class="backk">
-			<div class="carousel">
+			<section class="carousel">
 <?php
  
 //let's say each article costs 15.00 bucks
@@ -99,7 +98,6 @@ catch (Stripe_InvalidRequestError $e) {
 // Something else happened, completely unrelated to Stripe
 }
 ?>
-				
 				<div class="reel">
 
 				  <article>
@@ -660,9 +658,6 @@ catch (Stripe_InvalidRequestError $e) {
 					</article>
 
 				</div>
-			
-				
-			</div>
 			</section>
 			
 		<!-- Main -->
@@ -709,7 +704,7 @@ catch (Stripe_InvalidRequestError $e) {
                         
                         
                         More </br>
-                        There are actually three different herbs commonly called ginseng: Asian or Korean ginseng (Panax ginseng), American ginseng (Panax quinquefolius), and Siberian ‚Äúginseng‚Äù (Eleutherococcus senticosus), the latter herb having many of the same effects as the first two even though it is in a different plant family. American ginseng is grown in the Eastern portion of North America, along the entire eastern seaboard, from Quebec to Florida. Unlike Asian ginseng, which has ‚Äúwarming‚Äù properties, American ginseng has ‚Äúcooling‚Äù properties and is noted for its thirst quenching effects. Native Americans in North America used it in the exact same way as the Chinese, as a preventative.
+                        There are actually three different herbs commonly called ginseng: Asian or Korean ginseng (Panax ginseng), American ginseng (Panax quinquefolius), and Siberian ìginsengî (Eleutherococcus senticosus), the latter herb having many of the same effects as the first two even though it is in a different plant family. American ginseng is grown in the Eastern portion of North America, along the entire eastern seaboard, from Quebec to Florida. Unlike Asian ginseng, which has ìwarmingî properties, American ginseng has ìcoolingî properties and is noted for its thirst quenching effects. Native Americans in North America used it in the exact same way as the Chinese, as a preventative.
 						</p>
 						<p>
                         The use of any ginseng may reduce the efficacy of certain precription drugs, please talk to your doctor or healthcare professional.
@@ -723,9 +718,9 @@ catch (Stripe_InvalidRequestError $e) {
 							</header>
 							<!--<p>
 								Plant Family: Solanaceae. </br>
-                                It has been used both as a food and a medicine by Native Americans for over 9000 years. The Capsicum family includes bell peppers, red peppers, paprika, and pimento, but the most famous medicinal members of the family are cayenne and chile. The tasty hot peppers have long been used in many of the world‚Äôs cuisines, but their greatest use in health comes from, surprisingly, conventional medicine. </p>
+                                It has been used both as a food and a medicine by Native Americans for over 9000 years. The Capsicum family includes bell peppers, red peppers, paprika, and pimento, but the most famous medicinal members of the family are cayenne and chile. The tasty hot peppers have long been used in many of the worldís cuisines, but their greatest use in health comes from, surprisingly, conventional medicine. </p>
                           <p>
-                                Pepper in any form may be an irritant to the mucous membranes and caution should be exercised when handling. Don‚Äôt touch your eyes with your hands after you have handled capsaicin in any form as painful burning may occur. Excessive use internally may result in gastro-intestinal upset.
+                                Pepper in any form may be an irritant to the mucous membranes and caution should be exercised when handling. Donít touch your eyes with your hands after you have handled capsaicin in any form as painful burning may occur. Excessive use internally may result in gastro-intestinal upset.
 
 For educational purposes only This information has not been evaluated by the Food and Drug Administration.
 
@@ -741,12 +736,12 @@ This information is not intended to diagnose, treat, cure, or prevent any diseas
                             Plant Family: Zingiberaceae </br>
                             Ginger is the most widely used and available herbal remedy on the planet, with billions of people using it every day as both food and medicine. A tropical perennial growing to a height of two feet, ginger has lance-shaped leaves and bears stalks of white or yellow flowers.
 
-Ginger has long been the subject of fable and literature. For centuries, Europeans obtained ginger form Arab spice traders, who protected their sources by inviting stories of ginger field located in lands stalked by a fierce people called troglodytes. And Shakespeare wrote in Love‚Äôs Labour Lost, ‚Äúhad I but one penny in the world thou shouldst have it to buy ginger-bread.‚Äù
+Ginger has long been the subject of fable and literature. For centuries, Europeans obtained ginger form Arab spice traders, who protected their sources by inviting stories of ginger field located in lands stalked by a fierce people called troglodytes. And Shakespeare wrote in Loveís Labour Lost, ìhad I but one penny in the world thou shouldst have it to buy ginger-bread.î
 
 Ginger is used either fresh or dried in nearly two thirds of all traditional Chinese and Japanese herbal formulas. Fresh ginger is used to relieve dryness and heat, while dried ginger is used to relieve dampness and chill.
 </p>
 <p>
-Although there are warnings in both Traditional Chinese Medicine and some medical texts about using ginger during pregnancy, ginger used in moderation, that is 3/4 teaspoon up to three times a day, poses no risk to mother‚Äôs or baby‚Äôs health. Studies in the late 1990‚Äôs found that eating as much as 2 to 3 tablespoons of raw ginger or 5 to 8 tablespoons of dried ginger daily will not stimulate uterine contraction. Excessive use may cause gastro-intestinal upset.
+Although there are warnings in both Traditional Chinese Medicine and some medical texts about using ginger during pregnancy, ginger used in moderation, that is 3/4 teaspoon up to three times a day, poses no risk to motherís or babyís health. Studies in the late 1990ís found that eating as much as 2 to 3 tablespoons of raw ginger or 5 to 8 tablespoons of dried ginger daily will not stimulate uterine contraction. Excessive use may cause gastro-intestinal upset.
                             
                           </p>-->
 						</article>
